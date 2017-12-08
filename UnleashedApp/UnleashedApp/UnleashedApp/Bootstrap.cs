a582468bf92f940219a0750962b9ed7ec8f77600
Unknown <renemartinjacobs@hotmail.com>
2017-12-08 11:38:46 +0100

    Added bootstrap

--- UnleashedApp/UnleashedApp/UnleashedApp/Bootstrap.cs
+++ UnleashedApp/UnleashedApp/UnleashedApp/Bootstrap.cs
@@ -1,13 +1,38 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnleashedApp.Contracts.ViewModels;
+using UnleashedApp.ViewModels;
 
 namespace UnleashedApp
 {
     public class Bootstrap
     {
+        public IContainer CreateContainer()
+        {
+            var containerBuilder = new ContainerBuilder();
+            RegisterDependencies(containerBuilder);
+            return containerBuilder.Build();
+        }
 
+        protected virtual void RegisterDependencies(ContainerBuilder containerBuilder)
+        {
+            //services
+            containerBuilder.RegisterType<NavigationService>()
+                .As<INavigationService>()
+                .SingleInstance();
+
+            //ViewModels
+            containerBuilder.RegisterType<MenuViewModel>()
+                .As<IMenuViewModel>()
+                .SingleInstance();
+
+            containerBuilder.RegisterType<WhoIsWhoViewModel>()
+                .As<IWhoIsWhoViewModel>()
+                .SingleInstance();
+        }
     }
 }
