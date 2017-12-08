c4c73a30d02dc8032e71ab3cf64eb3fd04bff82c
Unknown <renemartinjacobs@hotmail.com>
2017-12-08 10:36:11 +0100

    Added a base class for the ViewModels

---
+++ UnleashedApp/UnleashedApp/UnleashedApp/ViewModelLocator.cs
@@ -0,0 +1,22 @@
+﻿using UnleashedApp.ViewModels;
+
+namespace UnleashedApp
+{
+    public class ViewModelLocator
+    {
+        //singelton
+        public static readonly ViewModelLocator Instance = new ViewModelLocator();
+
+        public MenuViewModel MenuViewModel { get; }
+
+        public ViewModelLocator()
+        {
+            //services:
+            INavigationService navigationService = new NavigationService();
+            //repositories:
+
+            //viewmodels:
+            MenuViewModel = new MenuViewModel(navigationService);
+        }
+    }
+}
--- UnleashedApp/UnleashedApp/UnleashedApp/ViewModels/MenuViewModel.cs
+++ UnleashedApp/UnleashedApp/UnleashedApp/ViewModels/MenuViewModel.cs
@@ -1,11 +1,27 @@
 ﻿using System.Windows.Input;
 using UnleashedApp.Contracts.ViewModels;
+using UnleashedApp.Views;
+using Xamarin.Forms;
 
 namespace UnleashedApp.ViewModels
 {
-    public class MenuViewModel : IMenuViewModel
+    public class MenuViewModel : ViewModelBase, IMenuViewModel
     {
         //properties
         public ICommand WhoIsWhoCommand { get; set; }
+
+        public MenuViewModel(INavigationService navigationService)
+        {
+            InitialiseComponents(navigationService);
+            InitialiseCommands();
+        }
+
+        private void InitialiseCommands()
+        {
+            WhoIsWhoCommand = new Command(async() =>
+            {
+                await NavigationService.PushAsync(nameof(WhoIsWhoView));
+            });
+        }
     }
 }
