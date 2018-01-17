5ea24f430455c71a62012ec3a2eaa45b8b6bad29
hannecolaers <hannecolaers@hotmail.com>
2018-01-17 16:15:08 +0100

    adds GoogleAuthenticator

--- UnleashedApp/UnleashedApp/UnleashedApp.Android/MainActivity.cs
+++ UnleashedApp/UnleashedApp/UnleashedApp.Android/MainActivity.cs
@@ -1,10 +1,6 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace UnleashedApp.Droid
@@ -20,6 +16,10 @@ namespace UnleashedApp.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            //Google login sreen
+            global::Xamarin.Auth.Presenters.XamarinAndroid.AuthenticationConfiguration.Init(this, bundle);
+
             LoadApplication(new App());
         }
     }
--- UnleashedApp/UnleashedApp/UnleashedApp/Views/LoginView.xaml.cs
+++ UnleashedApp/UnleashedApp/UnleashedApp/Views/LoginView.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Auth;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,10 +12,49 @@ namespace UnleashedApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginView : ContentPage
     {
+        OAuth2Authenticator authenticator;
         public LoginView()
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+
+            authenticator = new OAuth2Authenticator(
+                "213697302597-efe8ekru9nue7ihl2qocfbe0j8dgmqcp.apps.googleusercontent.com",
+                null,
+                "email",
+                new Uri("https://accounts.google.com/o/oauth2/v2/auth"),
+                new Uri("be.unleashed.app:/oauth2redirect"),
+                new Uri("https://www.googleapis.com/oauth2/v4/token"),
+                null,
+                true);
+            authenticator.Completed += OnAuthCompleted;
+
+            loginButton.Clicked += PresentGoogleLoginScreen;
         }
+
+        private void PresentGoogleLoginScreen(object sender, EventArgs e)
+        {
+            var presenter = new Xamarin.Auth.Presenters.OAuthLoginPresenter();
+            presenter.Login(authenticator);
+        }
+
+        private void OnAuthCompleted(object sender, AuthenticatorCompletedEventArgs e)
+        {
+            DisplayAlert("auth completed", "auth completed", "xx");
+            if (e.IsAuthenticated)
+            {
+                // The user is authenticated
+                // Extract the OAuth token
+                var token_type = e.Account.Properties["token_type"];
+                var accessToken = e.Account.Properties["access_token"];
+                DisplayAlert("is authenticated", "is authenticated", "xx");
+                // Do something
+            }
+            else
+            {
+                // The user is not authenticated
+            }
+        }
+
     }
 }
