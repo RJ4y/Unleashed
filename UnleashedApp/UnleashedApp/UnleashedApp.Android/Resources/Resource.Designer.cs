bf3a04d71ec750cc2dfa05257287ab9a6df93c85
Yannick <yannick.franssen@outlook.com>
2018-01-25 13:21:21 +0100

    Implement Splashscreen

--- UnleashedApp/UnleashedApp/UnleashedApp.Android/Resources/Resource.Designer.cs
+++ UnleashedApp/UnleashedApp/UnleashedApp.Android/Resources/Resource.Designer.cs
@@ -2192,26 +2192,26 @@ namespace UnleashedApp.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020111
-			public const int avd_hide_password_1 = 2130837777;
-			
-			// aapt resource value: 0x7f020112
-			public const int avd_hide_password_2 = 2130837778;
-			
 			// aapt resource value: 0x7f020113
-			public const int avd_hide_password_3 = 2130837779;
+			public const int avd_hide_password_1 = 2130837779;
+			
+			// aapt resource value: 0x7f020114
+			public const int avd_hide_password_2 = 2130837780;
+			
+			// aapt resource value: 0x7f020115
+			public const int avd_hide_password_3 = 2130837781;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020114
-			public const int avd_show_password_1 = 2130837780;
-			
-			// aapt resource value: 0x7f020115
-			public const int avd_show_password_2 = 2130837781;
-			
 			// aapt resource value: 0x7f020116
-			public const int avd_show_password_3 = 2130837782;
+			public const int avd_show_password_1 = 2130837782;
+			
+			// aapt resource value: 0x7f020117
+			public const int avd_show_password_2 = 2130837783;
+			
+			// aapt resource value: 0x7f020118
+			public const int avd_show_password_3 = 2130837784;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2759,11 +2759,11 @@ namespace UnleashedApp.Droid
 			// aapt resource value: 0x7f02010a
 			public const int notification_icon_background = 2130837770;
 			
-			// aapt resource value: 0x7f02010f
-			public const int notification_template_icon_bg = 2130837775;
+			// aapt resource value: 0x7f020111
+			public const int notification_template_icon_bg = 2130837777;
 			
-			// aapt resource value: 0x7f020110
-			public const int notification_template_icon_low_bg = 2130837776;
+			// aapt resource value: 0x7f020112
+			public const int notification_template_icon_low_bg = 2130837778;
 			
 			// aapt resource value: 0x7f02010b
 			public const int notification_tile_bg = 2130837771;
@@ -2775,7 +2775,13 @@ namespace UnleashedApp.Droid
 			public const int placeholder = 2130837773;
 			
 			// aapt resource value: 0x7f02010e
-			public const int unleashed = 2130837774;
+			public const int splash = 2130837774;
+			
+			// aapt resource value: 0x7f02010f
+			public const int splashscreen = 2130837775;
+			
+			// aapt resource value: 0x7f020110
+			public const int unleashed = 2130837776;
 			
 			static Drawable()
 			{
@@ -4330,6 +4336,9 @@ namespace UnleashedApp.Droid
 			
 			// aapt resource value: 0x7f0b018a
 			public const int MainTheme_Base = 2131427722;
+			
+			// aapt resource value: 0x7f0b018c
+			public const int MyTheme_Splash = 2131427724;
 			
 			// aapt resource value: 0x7f0b002d
 			public const int Platform_AppCompat = 2131427373;
