a1edffeb8313f2d32ebf4122cd75747c03c5cd90
Yannick <yannick.franssen@outlook.com>
2018-02-07 16:53:34 +0100

    Cleanup general Who is Who interface

--- UnleashedApp/UnleashedApp/UnleashedApp/Views/WhoIsWhoView.xaml.cs
+++ UnleashedApp/UnleashedApp/UnleashedApp/Views/WhoIsWhoView.xaml.cs
@@ -15,20 +15,10 @@ namespace UnleashedApp.Views
 
         void HabitatButtonClicked(object sender, EventArgs args)
         {
-            Button button = (Button)sender;
-            button.BackgroundColor = Color.DodgerBlue;
-            button.TextColor = Color.White;
-            SquadButton.BackgroundColor = Color.White;
-            SquadButton.TextColor = Color.DodgerBlue;
         }
 
         private void SquadButtonClicked(object sender, EventArgs e)
         {
-            Button button = (Button)sender;
-            button.BackgroundColor = Color.DodgerBlue;
-            button.TextColor = Color.White;
-            HabitatButton.BackgroundColor = Color.White;
-            HabitatButton.TextColor = Color.DodgerBlue;
         }
     }
 }
