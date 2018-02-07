266d7f22d3b71c5e25f384db0e215b1fb7dca3cc
Unknown <renemartinjacobs@hotmail.com>
2018-02-07 12:37:27 +0100

    Added tests for DateConverter

---
+++ UnleashedApp/UnleashedApp.Tests/Converters/DateConverterUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+using UnleashedApp.Converters;
+
+namespace UnleashedApp.Tests.Converters
+{
+    [TestFixture]
+    public class DateConverterUnitTests
+    {
+        [Test]
+        public void ConvertShouldReturnCorrectDateFormat()
+        {
+            //format of dd/MM/yyyy
+            var converter = new DateConverter();
+            var date = new DateTime(1952, 02, 08);
+
+            var result = converter.Convert(date, typeof(DateTime), "", CultureInfo.CurrentCulture);
+
+            Assert.AreEqual("08/02/1952", result.ToString());
+        }
+
+        [Test]
+        public void ConvertBackShouldReturnSameValue()
+        {
+            //format of dd/MM/yyyy
+            var converter = new DateConverter();
+            var date = new DateTime(1952, 02, 08);
+
+            var result = converter.ConvertBack(date, typeof(DateTime), "", CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(date, result);
+        }
+    }
+}
