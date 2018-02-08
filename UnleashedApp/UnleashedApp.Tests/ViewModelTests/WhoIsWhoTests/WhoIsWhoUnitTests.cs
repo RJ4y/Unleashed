e12518d83711580bb685f8cf4c8d230693bb042f
Unknown <renemartinjacobs@hotmail.com>
2018-02-08 08:57:53 +0100

    Removed redundant code

--- UnleashedApp/UnleashedApp.Tests/ViewModelTests/WhoIsWhoTests/WhoIsWhoUnitTests.cs
+++ UnleashedApp/UnleashedApp.Tests/ViewModelTests/WhoIsWhoTests/WhoIsWhoUnitTests.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnleashedApp.Tests.WhoIsWhoTests
+namespace UnleashedApp.Tests.ViewModelTests.WhoIsWhoTests
 {
     /// <summary>
     /// Summary description for WhoIsWhoUnitTests
@@ -11,53 +8,6 @@ namespace UnleashedApp.Tests.WhoIsWhoTests
     [TestClass]
     public class WhoIsWhoUnitTests
     {
-        public WhoIsWhoUnitTests()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-
         [TestMethod]
         public void TestMethod1()
         {
