3a0d298443c3a5c5729613cc83dd817e9cc10b2c
Unknown <renemartinjacobs@hotmail.com>
2018-02-09 11:02:50 +0100

    Test NameGameViewModel

---
+++ UnleashedApp/UnleashedApp.Tests/ViewModelTests/NameGameTests/NameGameUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using UnleashedApp.Models;
+using UnleashedApp.Repositories.EmployeeRepositories;
+using UnleashedApp.ViewModels;
+
+namespace UnleashedApp.Tests.ViewModelTests.NameGameTests
+{
+    [TestFixture]
+    public class NameGameUnitTests
+    {
+        private NameGameViewModel _nameGameViewModel;
+        private Mock<IEmployeeRepository> _employeeRepoMock;
+
+        [Test]
+        public void ConstructorShouldInitListOfEmployees()
+        {
+            var list = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Fons"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Jan"
+                },
+                new Employee
+                {
+                    Id = 3,
+                    FirstName = "Karel"
+                }
+            };
+
+            _employeeRepoMock = new Mock<IEmployeeRepository>();
+            _employeeRepoMock.Setup(x => x.GetAllEmployees()).Returns(list);
+
+            _nameGameViewModel = new NameGameViewModel(_employeeRepoMock.Object);
+
+            Assert.IsNotNull(_nameGameViewModel.Employees);
+            Assert.AreEqual(list, _nameGameViewModel.Employees);
+        }
+    }
+}
