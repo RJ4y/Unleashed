d0f57a6c87f33cf49a922b00aedf63e0423035a2
Unknown <renemartinjacobs@hotmail.com>
2018-02-09 10:40:11 +0100

    Tests voor RoomViewModel

---
+++ UnleashedApp/UnleashedApp.Tests/ViewModelTests/RoomTests/RoomUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using NUnit.Framework;
+using UnleashedApp.Contracts;
+using UnleashedApp.Repositories.EmployeeRepositories;
+using UnleashedApp.ViewModels;
+
+
+namespace UnleashedApp.Tests.ViewModelTests.RoomTests
+{
+    [TestFixture()]
+    public class RoomUnitTests
+    {
+        private Mock<INavigationService> _navigationMock;
+        private Mock<IEmployeeRepository> _employeeRepoMock;
+        private RoomViewModel _roomViewModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            _navigationMock = new Mock<INavigationService>();
+            _employeeRepoMock = new Mock<IEmployeeRepository>();
+            _roomViewModel = new RoomViewModel(_navigationMock.Object, _employeeRepoMock.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _navigationMock = null;
+            _employeeRepoMock = null;
+            _roomViewModel = null;
+        }
+
+        [Test]
+        public void ConstructorShouldInitCommand()
+        {
+            Assert.IsNotNull(_roomViewModel.EmployeeDetailCommand);
+        }
+
+        [Test]
+        public void ConstructorShouldInitEmployeeRepo()
+        {
+            Assert.IsNotNull(_roomViewModel.EmployeeRepository);
+            Assert.AreEqual(_employeeRepoMock.Object, _roomViewModel.EmployeeRepository);
+        }
+    }
+}
