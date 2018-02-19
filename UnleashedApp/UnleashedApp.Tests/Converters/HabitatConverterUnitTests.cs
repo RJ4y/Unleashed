382a543c5cc0853b2bf66c6759472c7e93abba7d
Unknown <renemartinjacobs@hotmail.com>
2018-02-19 14:18:19 +0100

    Added HabitatConverterTest

---
+++ UnleashedApp/UnleashedApp.Tests/Converters/HabitatConverterUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+using UnleashedApp.Converters;
+using UnleashedApp.Models;
+
+namespace UnleashedApp.Tests.Converters
+{
+    [TestFixture]
+    public class HabitatConverterUnitTests
+    {
+        [Test]
+        public void ConvertBackShouldReturnId()
+        {
+            var converter = new HabitatConverter();
+
+            var habitat = new Habitat
+            {
+                Id = 1,
+                Name = "Habitat"
+            };
+
+            var result = converter.ConvertBack(habitat, typeof(int), "", CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
