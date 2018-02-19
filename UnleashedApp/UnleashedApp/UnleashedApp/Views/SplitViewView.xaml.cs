e17b5db556f67355c87660e3ec5ac0a415503106
Yannick <yannick.franssen@outlook.com>
2018-02-19 12:14:21 +0100

    Update menu with new icon, rename items

--- UnleashedApp/UnleashedApp/UnleashedApp/Views/SplitViewView.xaml.cs
+++ UnleashedApp/UnleashedApp/UnleashedApp/Views/SplitViewView.xaml.cs
@@ -24,8 +24,8 @@ namespace UnleashedApp.Views
 
             List<MasterItem> menuList = new List<MasterItem>();
 
-            var nameGamePage = new MasterItem() { Title = "Home", Icon = "home.png", TargetType = typeof(NameGameView) };
-            var whoIsWhoPage = new MasterItem() { Title = "Who is Who", Icon = "users.png", TargetType = typeof(WhoIsWhoView) };
+            var nameGamePage = new MasterItem() { Title = "Who is Who", Icon = "home.png", TargetType = typeof(NameGameView) };
+            var whoIsWhoPage = new MasterItem() { Title = "Employees", Icon = "users.png", TargetType = typeof(WhoIsWhoView) };
             var floorPlanPage = new MasterItem() { Title = "Floor Plan", Icon = "map.png", TargetType = typeof(FloorplanView) };
             var trainingPage = new MasterItem() { Title = "Training", Icon = "training.png", TargetType = typeof(TrainingView) };
             var aboutPage = new MasterItem() { Title = "About" };
