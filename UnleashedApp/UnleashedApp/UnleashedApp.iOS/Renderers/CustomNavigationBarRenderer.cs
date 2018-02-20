70ffda2e54a5a6b3f6c772831ebf47ae604e1e3c
hannecolaers <hannecolaers@hotmail.com>
2018-02-20 09:34:51 +0100

    add logo, add menu icon

---
+++ UnleashedApp/UnleashedApp/UnleashedApp.iOS/Renderers/CustomNavigationBarRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyApp.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+namespace UnleashedApp.iOS.Renderers
+{
+    public class CustomNavigationBarRenderer : NavigationRenderer
+    {
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            NavigationBar.TopItem.TitleView = new UIImageView(UIImage.FromBundle("unleashed.png"))
+            {
+                ContentMode = UIViewContentMode.ScaleAspectFit
+            };
+
+
+        }
+
+    }
+}
