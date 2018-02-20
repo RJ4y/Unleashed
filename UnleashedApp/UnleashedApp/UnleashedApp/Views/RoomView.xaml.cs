057393b1af2542b76c2e7792432256b7a0ed5346
Yannick <yannick.franssen@outlook.com>
2018-02-20 11:26:17 +0100

    Remove LegendGrid from room view

--- UnleashedApp/UnleashedApp/UnleashedApp/Views/RoomView.xaml.cs
+++ UnleashedApp/UnleashedApp/UnleashedApp/Views/RoomView.xaml.cs
@@ -20,7 +20,6 @@ namespace UnleashedApp.Views
             Room = TransferService.GetSelectedRoom();
             Spaces = TransferService.GetSelectedSpaces();
             RoomNameLabel.Text = Room.Name;
-            CreateLegendGrid();
             CreateRoomGrid();
         }
 
@@ -40,23 +39,6 @@ namespace UnleashedApp.Views
                 RoomGrid.HeightRequest = width / amountOfColumns * amountOfRows;
             }
         }
-
-        #region LegendGrid
-        private void CreateLegendGrid()
-        {
-            LegendGridService.CreateLegendGridColumnDefinitions(LegendGrid);
-            LegendGridService.CreateLegendGridRowDefinitions(LegendGrid, 2);
-            FillLegendGrid();
-        }
-
-        private void FillLegendGrid()
-        {
-            GridService.AddColorLabel(LegendGrid, 1, 1, Color.Black, false);
-            GridService.AddTextLabel(LegendGrid, 1, 2, "Workspot", false);
-            GridService.AddColorLabel(LegendGrid, 2, 1, Room.Color, false);
-            GridService.AddTextLabel(LegendGrid, 2, 2, "Empty", false);
-        }
-        #endregion
 
         #region RoomGrid
         private void CreateRoomGrid()
